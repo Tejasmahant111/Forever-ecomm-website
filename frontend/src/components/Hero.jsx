d1c0e0d28a072034864652c98089385665bacff1
tejas mahant <Tejas Mahant>
2025-07-03 22:16:58 +0530

    Initial commit

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='flex flex-col sm:flex-row border-gray-400'>
+        {/*hero left side */}
+        <div className='w-full sm:w-1/2'>
+
+        </div>
+    
+    </div>
+  )
+}
+
+export default Hero
